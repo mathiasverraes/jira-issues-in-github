822f7148ed647dd418e9a2d37a0a2ad4e85cc102
kolja <koljaz@web.de>
2016-12-17 19:56:52 +0100

    Attached link replacing to end of pjax content replacing event.

--- content.js
+++ content.js
@@ -1,22 +1,31 @@
 
 chrome.extension.sendRequest({method: "getLocalStorage", key: "jira"}, function(response) {
-	var pattern = /([A-Z]{2,}-\d+)/g;
-	var jira = response.data + '/browse/';
 
-    // selectors in which to look for jira issues
-	var selectors = $('h2.content-title, p.commit-title');
+	const replaceLinks = function() {
+        const pattern = /([A-Z]{2,}-\d+)/g;
+        const jira = response.data + '/browse/';
 
-    selectors.each(function(){
-		var self = $(this);
-		if(self.attr("tagName") == 'A') { return; }
-		
-		var match = pattern.exec(self.text());  
-		if(match[1]) {
-		    // @todo this doesn't work with multiple matches in the same text
-			self.html($(this).text().replace(
-				pattern,
-				' <a style="color:#00775a" target="_blank" href="' + jira + match[1] + '">' + match[1] + '</a>'
-			));
-		}
+        // selectors in which to look for jira issues
+        const selectors = $('h1.gh-header-title, p.commit-title');
+
+        selectors.each(function(){
+            var self = $(this);
+            if(self.attr("tagName") == 'A') { return; }
+
+            var match = pattern.exec(self.text());
+            if(match[1]) {
+                // @todo this doesn't work with multiple matches in the same text
+                self.html($(this).text().replace(
+                    pattern,
+                    ' <a style="" target="_blank" href="' + jira + match[1] + '">' + match[1] + '</a>'
+                ));
+            }
+        });
+	};
+
+    $('#js-repo-pjax-container').on('pjax:end', function() {
+        replaceLinks();
 	});
+
+    replaceLinks();
 });
